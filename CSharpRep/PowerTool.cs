6977894c3c33f6bb8bdf010ef39843a6f5330d23
Martin Blomberg <martin.blomberg@outlook.com>
2017-11-24 15:29:03 +0100

    PowerTools

---
+++ CSharpRep/PowerTool.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpRep
+{
+    public class Hammer : PowerTool
+    {
+        private bool _canDrill;
+
+        public override bool CanDrill
+        {
+            get { return _canDrill; }
+            set { _canDrill = value; }
+        }
+
+        public override bool Use()
+        {
+            Console.WriteLine("Hammering away");
+            return true;
+        }
+    }
+
+    public class ElectricDrill : PowerTool, IChargeable
+    {
+        private bool _canDrill = true;
+        private int charge = 10;
+
+        public override bool CanDrill
+        {
+            get { return _canDrill; }
+            set { _canDrill = value; }
+        }
+
+        public void Charge()
+        {
+            Console.WriteLine("Charging....");
+            charge = 10;
+        }
+
+        public override bool Use()
+        {
+            if (charge > 0)
+            {
+                Console.WriteLine("Drilling away");
+                charge--;
+                return true;
+            }
+           
+            Console.WriteLine(".... No Charge");
+            return false;
+           
+        }
+    }
+
+    public abstract class PowerTool
+    {
+        public abstract bool CanDrill { get; set; }
+        public virtual bool Use()
+        {
+            Console.WriteLine("Should be overridden in subclasses");
+            throw new NotImplementedException();
+        }
+    }
+
+    public interface IChargeable
+    {
+        void Charge();
+    }
+}
--- CSharpRep/Program.cs
+++ CSharpRep/Program.cs
@@ -1,12 +1,66 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CSharpRep
 {
     class Program
     {
+        int[] array = new int[10];
+        int value;
+
+        public int ListMin(int[] list)
+        {
+            int minvalue = Int32.MaxValue;
+
+            for (int i = 0; i < list.Length; i++)
+                if (list[i] < minvalue)
+                    minvalue = list[i];
+            return minvalue;
+        }
+
+        public float CircleArea(ushort radius)
+        {
+            return 3.14f * radius * radius;
+        }
+
+        public void MyFunction(string param = "ojdå")
+        {
+            const int alpha = 1;
+            Int32 a = alpha;
+            List<Object> list = new List<Object>();
+
+            list.Add("String");
+            list.Add(alpha);
+
+            int b = a + alpha;
+
+            if (alpha + "" == param)
+            {
+
+            }
+        }
+
+        private void Run()
+        {
+            double d = 3.1;
+            string s = "Hej";
+
+            Program p = new Program();
+            int minValue = p.ListMin(p.array);
+            p.MyFunction("Hej");
+
+            ElectricDrill drill = new ElectricDrill();
+            PowerTool tool = new ElectricDrill();
+            Object anonymousTool = new ElectricDrill();
+            IChargeable chargeable = new ElectricDrill();
+            
+            Console.WriteLine(s);
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Program p = new Program();
+            p.Run();
         }
+
     }
 }
